5b8d2fa4680422a7b5214d007a1e94e3582a06b2
sam <sam.sandberg@gmail.com>
2013-02-14 22:44:34 +0100

    using gunicorn for deploying the app

--- webserver.fcgi
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-from flup.server.fcgi import WSGIServer
-from webserver import app
-
-if __name__ == '__main__':
-	WSGIServer(app, bindAddress='/var/run/bhattitype-fcgi.sock').run()
--- webserver.py
+++ webserver.py
@@ -1,12 +1,19 @@
 
+import sys
+
 from flask import Flask
 from flask import jsonify
 from flask import render_template
 from flask import request
+from gunicorn.app.base import Application
 
 from bhattitype import BhattiType
 
-PORT = 8080
+HOST = 'localhost' # Local
+#HOST = '0.0.0.0' # World available
+PORT = 8052
+WORKERS = 4
+
 app = Flask(__name__)
 
 
@@ -24,6 +31,30 @@ def api():
 	output = bt.convert(input)
 	return jsonify({'status': 'OK', 'input': input, 'output': output})
 
+
+def rungunicorn():
+	class WSGIServer(Application):
+		def init(self, parser, opts, args):
+			return {
+				'bind': '%s:%s' % (HOST, PORT),
+				'workers': WORKERS,
+				#'errorlog': LOGFILE,
+			}
+		def load(self):
+			return app
+	#sys.argv = [sys.argv[0]]
+	WSGIServer().run()
+
+def main():
+	if len(sys.argv) >= 2 and sys.argv[1] == 'test':
+		print "Running in debug mode at %s on port %s" % (HOST, PORT)
+		app.debug = True
+		app.run(host=HOST, port=PORT)
+		return
+
+	print "Running with gunicorn at %s on port %s" % (HOST, PORT)
+	rungunicorn()
+
+
 if __name__ == "__main__":
-	app.debug = True
-	app.run(host="0.0.0.0", port=PORT)
+	main()
